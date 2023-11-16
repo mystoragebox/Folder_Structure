15d13e181f6675918ce4532594bf16529aa5f5b6
mystoragebox <71749877+mystoragebox@users.noreply.github.com>
2023-11-16 11:36:00 +0700

    Update start.py

--- Scripts/CBR_to_CBZ/start.py
+++ Scripts/CBR_to_CBZ/start.py
@@ -1,1 +1,24 @@
-dfsghdfg
+# this simple script will convert cbr comic files into a cbz files. This script doesn't remove copy protection. You will need to create two new folders, 'input' and 'output' or change the variables below.
+
+# dependancies: patoolib
+
+import patoolib, os, time
+
+inputPath = 'input'
+outputPath = 'output'
+
+for comic in os.listdir(inputPath):
+
+    cSplit = os.path.splitext(comic)
+    
+    if cSplit[1].lower() != '.cbr':
+        print(comic + ' is not a cbr file, skipping.')
+        time.sleep(1)
+        continue
+
+    zipComic = cSplit[0] + '.zip'
+    cbzComic = cSplit[0] + '.cbz'
+
+    patoolib.repack_archive(os.path.join(inputPath, comic), os.path.join(outputPath, zipComic))
+
+    os.rename(os.path.join(outputPath, zipComic), os.path.join(outputPath, cbzComic))
